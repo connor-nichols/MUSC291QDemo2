b70afc27df3eda98f01c1b48833b98b634870b1b
Connor Nichols <cnichols9@huskers.unl.edu>
2022-01-20 13:13:54 -0600

    Initial Commit

---
+++ MUSC291QDemo/MUSC291QDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MUSC291QDemoApp.swift
+//  MUSC291QDemo
+//
+//  Created by Connor Nichols on 1/20/22.
+//
+
+import SwiftUI
+
+@main
+struct MUSC291QDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
